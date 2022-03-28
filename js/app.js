1e8d3113e4385d6dc43a8bc19d962a846f8db261
Matthew Carson <matthew.carson@pm.me>
2022-03-27 23:08:47 -0400

    Geocoding city names to coordinates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-alert('working!!!')
+const apiKey = '9f176747b923620b00a8bc8aa89846d4'
+
+const geocode = async city => {
+  // Geocoding Endpoint
+  let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
+
+  return await response.json()
+}
+
+(async () => {
+  const data = await geocode('Nuremberg')
+  document.querySelector('#city-name').textContent = data[0].name
+  document.querySelector('#lat').textContent = data[0].lat
+  document.querySelector('#long').textContent = data[0].lon
+})()
